43e5d14ccb431e84b06453700ef27da38394bd7d
gjsl19 <gjslre0319@gmail.com>
2023-08-14 19:05:02 +0900

    텍스트 정렬. 정리

--- website/first-app/src/components/Main/index.js
+++ website/first-app/src/components/Main/index.js
@@ -1,29 +1,34 @@
 import React, { useState } from "react";
-import { useParallax } from "react-scroll-parallax";
+import { ParallaxProvider, Parallax } from "react-scroll-parallax";
 import { ExampleRotation } from "./ExampleRotation";
 import "./style.scss";
 
 const Main = ({ id }) => {
-  const parallax =
-    useParallax <
-    HTMLDivElement >
-    {
-      rotateY: [0, 360],
-    };
-
   return (
     <>
       <div id={id}>
         <div className="main-wrap">
-          <div ref={parallax.ref} className="spinner group">
-            <h1>슬기로운 금융생활 페이워치 활용백서</h1>
-            <h2>세상에 이런 복지 봤어?</h2>
-            <p>
-              매달 월급날에 받을 급여 중 일부를 내가 원하고 필요할 때 먼저
-              인출할 수 있어요
-            </p>
-          </div>
-          <ExampleRotation />
+          <ParallaxProvider>
+            <div className="group">
+              <h1>
+                슬기로운 금융생활 <strong>페이워치 활용백서</strong>
+              </h1>
+
+              <Parallax scale={[0, 2]}>
+                <h2>세상에 이런 복지 봤어?</h2>
+              </Parallax>
+              <Parallax>
+                <p>
+                  매달 월급날에 받을 급여 중 일부를 내가 원하고 필요할 때 먼저
+                  인출할 수 있어요
+                </p>
+              </Parallax>
+            </div>
+
+            <Parallax translateY={-100} speed={100} className="group2">
+              <ExampleRotation />
+            </Parallax>
+          </ParallaxProvider>
         </div>
       </div>
     </>
