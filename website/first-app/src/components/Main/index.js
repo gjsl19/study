c30f9c3e3f5f6cd5c40050da395ce381caff7227
gjsl19 <gjslre0319@gmail.com>
2023-08-18 17:17:39 +0900

    리액트 스크롤 패럴렉스 라이브러리 삭제할거얌

--- website/first-app/src/components/Main/index.js
+++ website/first-app/src/components/Main/index.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import { ParallaxProvider, Parallax } from "react-scroll-parallax";
-import { ExampleRotation } from "./ExampleRotation";
+import React from "react";
+
 import "./style.scss";
 
 const Main = ({ id }) => {
@@ -8,27 +7,21 @@ const Main = ({ id }) => {
     <>
       <div id={id}>
         <div className="main-wrap">
-          <ParallaxProvider>
-            <div className="group">
-              <h1>
-                슬기로운 금융생활 <strong>페이워치 활용백서</strong>
-              </h1>
-
-              <Parallax scale={[0, 2]}>
-                <h2>세상에 이런 복지 봤어?</h2>
-              </Parallax>
-              <Parallax>
-                <p>
-                  매달 월급날에 받을 급여 중 일부를 내가 원하고 필요할 때 먼저
-                  인출할 수 있어요
-                </p>
-              </Parallax>
-            </div>
-
-            <Parallax translateY={-100} speed={100} className="group2">
-              <ExampleRotation />
-            </Parallax>
-          </ParallaxProvider>
+          <div className="group">
+            <h1>
+              슬기로운 금융생활 <strong>페이워치 활용백서</strong>
+            </h1>
+            <h2>세상에 이런 복지 봤어?</h2>
+            <p className="txt01">
+              월급 중 일부를 기다리지 않고
+              <br />
+              내가 원하고 필요할 때
+              <br />
+              먼저 인출할 수 있다?
+            </p>
+            <p className="">YES</p>
+            <p>NO</p>
+          </div>
         </div>
       </div>
     </>
